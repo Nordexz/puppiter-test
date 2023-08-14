c69d60f851ea644575ce2ef5d13a05a267de086e
nordexz <nordex83@gmail.com>
2023-08-14 13:35:10 +0300

    test

--- src/pages/api/printEcg/index.js
+++ src/pages/api/printEcg/index.js
@@ -1,18 +1,24 @@
 /* eslint-disable import/no-anonymous-default-export */
 // import puppeteer from '../../../../node_modules/puppeteer';
-let puppeteer = require('../../../../node_modules/puppeteer-core');
-// else puppeteer = require('../../../../node_modules/puppeteer');
+export const config = {
+  runtime: 'edge', 
+};
+
+let puppeteer
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) puppeteer = require('../../../../node_modules/puppeteer-core');
+else puppeteer = require('../../../../node_modules/puppeteer');
 const chromium = require('chrome-aws-lambda');
 
 export default async (req, res) => {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage({
+    const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
-      headless: chromium.headless,
+      headless: 'new    ',
     });
+    const page = await browser.newPage();
     
     await page.goto(req.body.query)
     const pdfFIle = await page.pdf({format: 'A4'})
