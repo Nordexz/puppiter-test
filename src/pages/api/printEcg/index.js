278f811919ced8ed0650563b9c3200c4bbbb1a87
nordexz <nordex83@gmail.com>
2023-08-14 15:30:18 +0300

    testing

--- src/pages/api/printEcg/index.js
+++ src/pages/api/printEcg/index.js
@@ -1,23 +1,15 @@
-let puppeteer;
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  puppeteer = require('../../../../node_modules/puppeteer-core');
-} else {
-  puppeteer = require('../../../../node_modules/puppeteer');
-}
+const puppeteer = require('puppeteer-core');
 const chromium = require('chrome-aws-lambda');
 
-const cache = new Map(); // Используем Map для хранения кэша
+const browserCache = new Map();
 
 async function launchChromium() {
-  const cachedConfig = cache.get('chromiumConfig');
-
-  if (cachedConfig) {
-    console.log('Using cached Chromium configuration');
-    return cachedConfig;
+  if (browserCache.has('browser')) {
+    console.log('Using cached Chromium browser');
+    return browserCache.get('browser');
   }
 
-  console.log('No cached Chromium configuration found. Launching Chromium...');
+  console.log('No cached Chromium browser found. Launching Chromium...');
 
   const browser = await puppeteer.launch({
     args: chromium.args,
@@ -26,25 +18,20 @@ async function launchChromium() {
     headless: 'new',
   });
 
-  const chromiumConfig = {
-    browser,
-  };
+  browserCache.set('browser', browser);
 
-  cache.set('chromiumConfig', chromiumConfig);
-
-  return chromiumConfig;
+  return browser;
 }
 
 export default async (req, res) => {
   try {
-    const { browser } = await launchChromium();
+    const browser = await launchChromium();
     const page = await browser.newPage();
 
     await page.goto(req.body.query);
     const pdfFile = await page.pdf({ format: 'A4' });
 
-    await browser.close();
-
+    await page.close(); // Закрываем страницу, чтобы освободить ресурсы
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=generated-pdf.pdf');
     res.send(pdfFile);
