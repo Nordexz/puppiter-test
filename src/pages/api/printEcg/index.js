f494b7d20103962a6bf600d1efcece0109cae177
nordexz <nordex83@gmail.com>
2023-08-14 13:37:33 +0300

    checking

--- src/pages/api/printEcg/index.js
+++ src/pages/api/printEcg/index.js
@@ -1,8 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 // import puppeteer from '../../../../node_modules/puppeteer';
-export const config = {
-  runtime: 'edge', 
-};
 
 let puppeteer
 
