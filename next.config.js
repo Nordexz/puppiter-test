cfb89aaca4b70545e8002db9ca54acd135f2dff8
nordexz <nordex83@gmail.com>
2023-08-14 13:11:08 +0300

    test

--- next.config.js
+++ next.config.js
@@ -18,17 +18,11 @@ module.exports = {
         'node_modules/@swc/core-linux-x64-gnu',
         'node_modules/@swc/core-linux-x64-musl',
         'node_modules/@esbuild/linux-x64',
-      ],
-    },
-  },
-  experimental: {  
-    outputFileTracingExcludes: {  
-      '*': [  
         'node_modules/@swc/**/*',  
         'node_modules/@esbuild/**/*',  
         'node_modules/terser/**/*',  
-        'node_modules/webpack/**/*',  
-      ],  
+        'node_modules/webpack/**/*',
+      ],
     },
-  }
+  },
 };
