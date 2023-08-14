f74f4dbe1313284563b4fe4f4e4c685ccb29b92c
nordexz <nordex83@gmail.com>
2023-08-14 11:09:01 +0300

    implement pdf downlaoder

---
+++ src/pages/api/printEcg/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+import puppeteer from '../../../../node_modules/puppeteer';
+import { executablePath } from '../../../../node_modules/puppeteer';
+const cheerio = require('cheerio');
+const chromium = require('chrome-aws-lambda');
+
+export default async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+    
+    await page.goto(req.body.query)
+    const pdfFIle = await page.pdf({format: 'A4'})
+
+    await browser.close();
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=generated-pdf.pdf');
+    res.send(pdfFIle);
+    
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
